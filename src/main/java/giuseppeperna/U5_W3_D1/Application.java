0c3c28e86f3c949c362a066c83cd03ac6a34f3f8
PernaGiuseppe <giuseppeperna97@hotmail.com>
2025-10-20 12:59:23 +0200

    Initial commit

---
+++ src/main/java/giuseppeperna/U5_W3_D1/Application.java
@@ -0,0 +1,13 @@
+package giuseppeperna.U5_W3_D1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
